e1067616fb2ddaefd9ea3ba5f115b9f547968120
universead <151545557+universead@users.noreply.github.com>
2023-12-01 00:58:47 +0000

    Update FirstBDD.java

--- src/test/java/FirstBDD.java
+++ src/test/java/FirstBDD.java
@@ -1,5 +1,5 @@
 public class FirstBDD {
     public static void main(String[] args) {
-        System.out.println("Hello hi and Namaste!!!!!");
+        System.outtt.println("Hello hi and Namaste!!!!!");
     }
 }
